c2c14b480dffd445eee464f40a7e810ec2251fe4
CAN <zeynabbabaei@Zeynabs-MacBook-Pro.local>
2022-04-08 09:29:47 -0400

    first git commit for my selenium code

---
+++ src/TestClass.java
@@ -0,0 +1,48 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class TestClass {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver", "/Users/zeynabbabaei/Desktop/Selenium-WebDriver/BrowserDrivers/chromedriver_new");
+
+		///WebDriver driver = new ChromeDriver();
+		
+		//driver.get("http://demo-store.seleniumacademy.com/vip.html");
+		///driver.get("http://demo-store.seleniumacademy.com/catalogsearch/result/index/?limit=24&q=pants");
+		//driver.manage().window().maximize();
+//		//driver.findElement(By.xpath("//*[@id=\"header\"]/div/div[2]/div/a")).click();
+//		driver.findElement(By.cssSelector("a[class$='skip-account']")).click();
+//		driver.findElement(By.cssSelector("a[title='Log In']")).click();
+//		driver.findElement(By.id("email")).sendKeys("safa.babaei@yahoo.com");
+//		Thread.sleep(2000);
+//		driver.findElement(By.id("email")).clear();
+//		//to get attribute value
+//		String typeValue = driver.findElement(By.id("email")).getAttribute("type");
+//		System.out.println("value of type = " + typeValue);
+//		 
+//		driver.navigate().to("http://demo-store.seleniumacademy.com/home-decor.html");
+//		String backgroundColor = driver.findElement(By.xpath("//button[@title='Subscribe']")).getCssValue("background");
+//		System.out.println("background Color = " + backgroundColor);
+//		
+//		
+//		System.out.println("size is " + driver.findElement(By.xpath("//button[@title='Subscribe']")).getSize());
+//		System.out.println("location is = " + 	driver.findElement(By.xpath("//button[@title='Subscribe']")).getLocation());
+//		System.out.println("text is  = " + driver.findElement(By.xpath("//button[@title='Subscribe']")).getText());
+//		System.out.println("tagName is  = " + driver.findElement(By.xpath("//button[@title='Subscribe']")).getTagName());
+//		System.out.println("is Displayed is  = " + driver.findElement(By.xpath("//button[@title='Subscribe']")).isDisplayed());
+//		System.out.println("enable is  = " + driver.findElement(By.xpath("//button[@title='Subscribe']")).isEnabled());
+//		System.out.println("Seleceted is  = " + driver.findElement(By.xpath("//button[@title='Subscribe']")).isSelected());
+//		
+	
+		
+	
+		
+		
+	}
+
+}
